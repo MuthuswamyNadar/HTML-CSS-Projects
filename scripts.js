71c15a9b227acdbb528f2722264d5bad5656b076
Muthuswamy <muthuswamynadar25@gmail.com>
2025-01-30 11:46:56 +0530

    added the nav bar

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "200px";
+    document.querySelector(".main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.querySelector(".main").style.marginLeft = "0";
+}
